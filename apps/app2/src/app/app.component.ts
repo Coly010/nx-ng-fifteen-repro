0bf8457d6009cc7836e449ebf76276124e6036ea
Colum Ferry <cferry09@gmail.com>
2022-11-07 11:34:26 +0000

    Initial commit

---
+++ apps/app2/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@nx-ng-fifteen-nest/api-interfaces';
+
+@Component({
+  selector: 'nx-ng-fifteen-nest-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>("/api/hello");
+  constructor(private http: HttpClient) {
+  }
+}
